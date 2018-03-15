1689fec9dde75cbef3895642781f4a47a3074d21
Tran Truong Vu <vutran95.it@gmail.com>
2018-03-15 15:18:53 +0700

    6. Shoot and Shot

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    private const string PLAYER_TAG = "Player";
+
+    [SerializeField]
+    private GameObject barrel;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    public PlayerWeapon weapon;
+
+    void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    [Client]
+    void Shoot ()
+    {
+        Vector3 forward = barrel.transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(barrel.transform.position, forward, Color.cyan, 2f);
+
+        RaycastHit _hit;
+        if (Physics.Raycast(barrel.transform.position, barrel.transform.forward, out _hit, weapon.Range, mask))
+        {
+            if (_hit.collider.tag == PLAYER_TAG)
+            {
+                CmdPlayerGotShot(_hit.collider.name);
+            }
+        }
+    }
+
+    // Server call Player who got shot
+    [Command]
+    void CmdPlayerGotShot (string _ID)
+    {
+        Debug.Log(_ID + " has been shot.");
+    }
+}
--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -7,17 +7,20 @@ public class PlayerSetup : NetworkBehaviour {
 
     [SerializeField]
     private Behaviour[] componentsToDisable;
+    [SerializeField]
+    private string remoteLayerName = "RemotePlayer";
 
     private Camera sceneCamera;
+    private Rigidbody rb;
 
     void Start ()
     {
+        rb = GetComponent<Rigidbody>();
+
         if (!isLocalPlayer)
         {
-            for (int i = 0; i < componentsToDisable.Length; i++)
-            {
-                componentsToDisable[i].enabled = false;
-            }
+            DisableComponents();
+            AssignRemoteLayer();
         }
         else
         {
@@ -27,8 +30,34 @@ public class PlayerSetup : NetworkBehaviour {
                 sceneCamera.gameObject.SetActive(false);
             }
         }
+        // Get player ID
+        RegisterPlayer();
     }
 
+    // Register player
+    void RegisterPlayer ()
+    {
+        string _ID = "Player" + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+        rb.useGravity = true;
+    }
+
+    // Assign RemotePlayer Layer
+    void AssignRemoteLayer ()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    // Disable components
+    void DisableComponents ()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+
+    // Re-Enable Scene Camera
     void OnDisable ()
     {
         if (sceneCamera != null)
