61f5300b89b824e67fe0d5295b50b1e4484fb954
Tran Truong Vu <vutran95.it@gmail.com>
2018-03-21 16:01:33 +0700

    2. Fix Move / Rotate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,65 +14,36 @@ public class PlayerController : MonoBehaviour {
     private float rotationSpeed = 1000f;
 
     private PlayerMotor playerMotor;
-    private CharacterController controller;
     private Camera cam;
+
+    private Vector3 inputMovement;
 
     void Start ()
     {
         playerMotor = GetComponent<PlayerMotor>();
-        controller = GetComponent<CharacterController>();
         cam = Camera.main;
     }
 
     void Update ()
     {
 
-        //Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-
-        //Vector3 motion = input;
-
-        //if (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1)
-        //{ motion *= 0.7f; }
-        //else
-        //{ motion *= 1f; }
-
-        //if (Input.GetButton("Run"))
-        //{ motion *= runSpeed; }
-        //else
-        //{ motion *= walkSpeed; }
-
-        //motion += Vector3.up * -8;
-        //controller.Move(motion * Time.deltaTime);
-
-        /////Calculate input for movement
-        float _xMove = Input.GetAxisRaw("Horizontal");
-        float _zMove = Input.GetAxisRaw("Vertical");
-        Vector3 _moveHorizontal = transform.right * _xMove;
-        Vector3 _moveVertical = transform.forward * _zMove;
+        //Calculate input for movement
+        Vector3 _velocity = new Vector3(Input.GetAxisRaw
+            ("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
         // Apply movement
         if (Input.GetButton("Run"))
-        {
-            Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * runSpeed;
-            playerMotor.Move(_velocity);
-        }
+            playerMotor.Move(_velocity *= runSpeed);
         else // walk
-        {
-            Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
-            playerMotor.Move(_velocity);
-        }
+            playerMotor.Move(_velocity *= walkSpeed);
 
-        // Make camera stop rotate
-        ///// Calculate input for Rotation
-        Vector3 mousePos = Input.mousePosition;
-        mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.transform.position.y - transform.position.y));
+        // Calculate input for Rotation
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        Vector3 currentPos = mousePos - new Vector3(transform.position.x, 0f, transform.position.z);
-        targetRotation = Quaternion.LookRotation(currentPos);
-        transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,
-            targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
-
-
+        // Apply Rotation
+        if (Physics.Raycast(ray, out hit))
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
     }
 
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -7,6 +7,7 @@ public class PlayerMotor : MonoBehaviour {
 
     private Rigidbody rgbd;
     private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
 
     void Start ()
     {
@@ -25,11 +26,13 @@ public class PlayerMotor : MonoBehaviour {
         velocity = _velocity;
     }
 
+    // Apply Movement
     private void PerformMovement ()
     {
         if (velocity != Vector3.zero)
         {
-            rgbd.MovePosition(rgbd.position + velocity * Time.fixedDeltaTime);
+            //rgbd.MovePosition(rgbd.position + velocity * Time.fixedDeltaTime);
+            transform.Translate(velocity * Time.fixedDeltaTime, Space.World);
         }
     }
 }
