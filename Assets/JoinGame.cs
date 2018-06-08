58829b8b4f5a53372bdac10aac63977cd76713ee
Vu.Tran <vutran95.it@gmail.com>
2018-06-08 14:12:44 +0700

    19. Add Join Room UI

---
+++ Assets/JoinGame.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame : MonoBehaviour {
+
+    List<GameObject> roomList = new List<GameObject>();
+
+    private NetworkManager networkManager;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Text statusTxt;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    // Start
+    void Start ()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+    }
+
+    public void RefreshRoomList ()
+    {
+        ClearRoomList();
+
+        networkManager.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+        statusTxt.text = "Loading . . .";
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        statusTxt.text = "";
+
+        if(matchList == null)
+        {
+            statusTxt.text = "Couldn't get any room.";
+            return;
+        }
+
+        foreach (MatchInfoSnapshot match in matchList)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+
+            // Add component on gameObject 
+            RoomListItem _roomListItem = GetComponent<RoomListItem>();
+            if (_roomListItem != null)
+            {
+                _roomListItem.Setup(match);
+            }
+
+            roomList.Add(_roomListItemGO);           
+        }
+        // When there is no room
+        if (roomList.Count == 0)
+        {
+            statusTxt.text = "No room at the moment.";
+        }
+    }
+
+    void ClearRoomList ()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]); 
+        }
+        roomList.Clear();
+    }
+}
---
+++ Assets/RoomListItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+public class RoomListItem : MonoBehaviour
+{
+    [SerializeField]
+    private Text roomNameTxt;
+
+    private MatchInfoSnapshot match;
+
+    public void Setup (MatchInfoSnapshot _match)
+    {
+        match = _match;
+
+        roomNameTxt.text = match.name + " (" + match.currentSize + " / " + match.maxSize + ")";
+    }
+	
+}
